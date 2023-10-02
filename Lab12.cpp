6f4fb7a499f615ee1ebf5c2dda97854c046dda49
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-10-02 19:44:02 -0400

    Create Lab12.cpp

---
+++ Lab12.cpp
@@ -0,0 +1,10 @@
+// Write a program that prompts the user to enter an integer to define the number of rows to be 
+// printed for a right triangle then use nested loops to iteratively print a pattern of asterisks to form a right triangle pattern.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
